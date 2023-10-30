b618fc92becb0f66d83982453c9f22890bd2cfc4
BURiley22 <47166709+BURiley22@users.noreply.github.com>
2023-10-30 09:08:14 -0700

    Basic Javascript Assignment
    This contains the requirements for the assignment from step 64 of the Javascript Course.

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+window.alert("Hello, World!");  //This will issue a pop-up when first arriving on the page
+
+//This is making use of the \ to enable in line quotation marks.
+document.write("Archer asked Dad, \"What is for dinner?\" \"Pizza, my dude!\" Dad responded enthusiastically."
++ " The two of them were over joyed at the prospect of pizza."
++ " \"What kind of toppings?\" Archer asked hoping it was only cheese.");
+
+
+//The following uses variables then concatentes the results.
+var A = "This is the beginning of the string "
+var B = "and this is the end of the string"
+document.write(A + B)
+
+//This section uses an array then a variable to access it.
+var Console = "Video Game System", PS5 = "Playstation 5", XB = "XBOX", NS = "Nintendo Switch", PC = "Computer"
+document.write(NS) 
+
+//A basic Javascript expression
+var C = 20
+var D = 15
+var E = document.write(C - D)
